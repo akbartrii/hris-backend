fdbe403778451c4a9ad27488b78fbba9c82dd548
ikrarnegaraa <akbar.tri@atibusinessgroup.com>
2026-05-15 15:19:40 +0700

    redeploy sin

--- scratch/verify_optimization.js
+++ scratch/verify_optimization.js
@@ -6,9 +6,9 @@ async function main() {
   const email = 'ahmad.spv@samugara.co.id';
 
   console.log(`Verifying eager loading optimization for: ${email}`);
-  
+
   try {
-    // This simulates the new query in login
+    // This simulates the new query in loginn
     const start = Date.now();
     const user = await prisma.ms_users.findUnique({
       where: { email },
@@ -34,7 +34,7 @@ async function main() {
     console.log('Employee ID:', user.ms_employees?.id);
     console.log('Location name:', user.ms_employees?.ms_locations?.name);
     console.log('WFH status:', user.ms_employees?.tr_remote_work_requests_current_remote_work?.status);
-    
+
     // Now verify we have all data needed for buildAssignedLocations
     const locations = [];
     const emp = user.ms_employees;
